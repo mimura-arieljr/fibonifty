9d22d3b8557258502e8e48e0ea2705040818bf33
chockmimura <avmimurajr@gmail.com>
2025-08-07 19:00:18 +0800

    FB: Initialize front and backend

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Index } from "./pages/Index";
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Index />}></Route>
+        </Routes>
+      </BrowserRouter>
+
+    </>
+  )
+}
+
+export default App
---
+++ client/src/pages/Index.tsx
@@ -0,0 +1,9 @@
+
+export const Index = () => {
+    return (
+        <div>
+            <h1>Welcome to Fibonifty</h1>
+            <p>Your go-to solution for agile planning.</p>
+        </div>
+    );
+};
